50599dc0da260e59e4a64b6832a468b29b3e311a
vittfiles <vittfiles@gmail.com>
2024-04-28 14:50:52 -0300

    se corrigio build

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-import { APIROUTES } from './config/API.routes'
+/* import { APIROUTES } from './config/API.routes' */
 import { APPROUTES } from './config/APP.routes'
 
 export async function middleware(request: NextRequest) {
